6aa7ece9afd9b7305a8a1e4ac6e92f4323bdc216
Bl00k25 <koronosensei51@gmail.com>
2025-04-03 00:59:03 +0300

    Toky/feature : connected the login page to the database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ManagerService } from 'app/services/manager.service';
 
 @NgModule({
   imports: [
@@ -22,7 +23,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     AppComponent,
     AdminLayoutComponent,
   ],
-  providers: [],
+  providers: [ManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
